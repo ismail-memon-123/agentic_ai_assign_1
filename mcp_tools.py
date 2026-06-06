041d7307071fc4d3c142dad993425700b8dbfa16
Ismail Memon <ismail.memon@vanderbilt.edu>
2026-06-06 00:22:19 -0700

    Create mcp_tools.py

---
+++ mcp_tools.py
@@ -0,0 +1,39 @@
+import requests
+from crewai.tools import BaseTool
+
+MCP_URL = "http://127.0.0.1:8080/mcp"
+
+class ReadCSVTool(BaseTool):
+
+    name: str = "Read CSV Tool"
+
+    description: str = "Reads a CSV preview"
+
+    def _run(self):
+
+        response = requests.post(
+            f"{MCP_URL}/tools/file_read_csv",
+            json={
+                "sample":90
+            }
+        )
+
+        return response.json()
+
+class ComputeStatsTool(BaseTool):
+
+    name: str = "Compute Stats Tool"
+
+    description: str = "Computes statistics"
+
+    def _run(self, rows, column):
+
+        response = requests.post(
+            f"{MCP_URL}/tools/compute_stats",
+            json={
+                "rows": rows,
+                "column": column
+            }
+        )
+
+        return response.json()
